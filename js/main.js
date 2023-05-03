be63a802c51d0a5775dac5de7cfb7390a1f8f81f
gabrielmussafiri <musgabriel2@gmail.com>
2023-05-03 09:42:07 +0200

    design updated

--- js/main.js
+++ js/main.js
@@ -2,3 +2,19 @@ let menu = document.querySelector('.menu-icon');
 menu.onclick = () => {
   menu.classList.toggle('move');
 };
+
+// Reviews Swiper
+const progressCircle = document.querySelector('.autoplay-progress svg');
+const progressContent = document.querySelector('.autoplay-progress span');
+var swiper = new Swiper('.reviews-content', {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: true,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
